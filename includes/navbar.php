2cf127e35e2ec2179e7d8d9bbfd914388fbbed7a
SirBen <akawuben@gmail.com>
2025-10-27 09:36:06 +0100

    Cleaned Scripts

--- includes/navbar.php
+++ includes/navbar.php
@@ -14,61 +14,19 @@
         <li class="menu-item">
           <a href="our-management?p=our-management">CORPORATE MANAGEMENT</a>
         </li>
-        <!-- <li class="menu-item">
-                          <a href="page-about-careers">ABOUT CAREERS</a>
-                        </li> -->
+       
       </ul>
     </li>
     <li class="menu-item <?php if (!empty($page) && $page == 'services') { ?>current-menu-item<?php } ?>">
       <a href="services/?p=services">SERVICES</a>
-      <!-- <ul class="sub-menu">
-                        <li class="menu-item">
-                          <a href="page-services">SERVICES</a>
-                        </li>
-                        <li class="menu-item">
-                          <a href="page-services-detail"
-                            >SERVICES DETAIL</a
-                          >
-                        </li>
-                      </ul> -->
+    
     </li>
     <li class="menu-item menu-item-has-children <?php if (!empty($page) && $page == 'projects') { ?>current-menu-item<?php } ?>">
       <a href="<?php if (!empty($_GET['p']) && ($_GET['p'] == 'services' || $_GET['p'] == 'projects'))  {?>/projects/?p=projects
             <?php } else{?>projects/?p=projects<?php } ?>">PROJECTS</a>
-      <!-- <ul class="sub-menu">
-        <li class="menu-item">
-          <a href="page-projects">PROJECTS</a>
-        </li>
-        <li class="menu-item">
-          <a href="page-projects-full">PROJECTS FULL WIDTH</a>
-        </li>
-        <li class="menu-item">
-          <a href="page-project-detail">PROJECTS DETAIL</a>
-        </li>
-      </ul> -->
+     
     </li>
-    <li class="menu-item menu-item-has-children">
-      <a href="page-testimonial">PAGE</a>
-      <ul class="sub-menu">
-        <li class="menu-item">
-          <a href="page-testimonial">PAGE TESTIMONIAL</a>
-        </li>
-        <li class="menu-item">
-          <a href="page-pricing">PAGE PRICING</a>
-        </li>
-      </ul>
-    </li>
-    <li class="menu-item menu-item-has-children">
-      <a href="page-blog">BLOG</a>
-      <ul class="sub-menu right-sub-menu">
-        <li class="menu-item">
-          <a href="page-blog">BLOG</a>
-        </li>
-        <li class="menu-item">
-          <a href="page-blog-single">BLOG SINGLE</a>
-        </li>
-      </ul>
-    </li>
+   
     <li class="menu-item <?php if (!empty($page) && $page == 'contactus') { ?>current-menu-item<?php } ?>">
       <a href="<?php if (!empty($_GET['p']) && $_GET['p'] == 'services') {?>/contact-us?p=contactus
             <?php } else{?>contact-us?p=contactus<?php } ?>">CONTACT</a>
